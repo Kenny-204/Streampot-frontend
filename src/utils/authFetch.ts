04015caec07ae3308d5e509ae0bb6eea7f1539c3
Kehinde <giwatwins@gmail.com>
2026-01-07 13:21:29 -0800

    bug fix

--- src/utils/authFetch.ts
+++ src/utils/authFetch.ts
@@ -1,10 +1,26 @@
+// export default async function authFetch(
+//   url: string,
+//   options = { headers: {}, method: "GET", body: undefined }
+// ) {
+//   const token = localStorage.getItem("token");
+//   return fetch(url, {
+//     ...options,
+//     headers: { Authorization: `Bearer ${token}`, ...options.headers },
+//   });
+// }
 export default async function authFetch(
   url: string,
-  options = { headers: {}, method: "GET", body: undefined }
+  options: RequestInit = {}
 ) {
   const token = localStorage.getItem("token");
+
+  const headers = {
+    ...(options.headers || {}),
+    ...(token ? { Authorization: `Bearer ${token}` } : {}),
+  };
+
   return fetch(url, {
     ...options,
-    headers: { Authorization: `Bearer ${token}`, ...options.headers },
+    headers,
   });
 }
